d89c581ad083fe118d79134e8283f7294754dfee
Ciro S. Costa <ciro.costa@usp.br>
2015-05-13 03:36:49 -0300

    starts shader code

--- core/src/utils/fileinput.h
+++ core/src/utils/fileinput.h
@@ -6,25 +6,30 @@
 
 namespace yengine {
 
-  std::string read_file(const char* filename)
+  class FileInput
   {
-    unsigned long length;
-    char* data;
-    FILE* file = fopen(filename, "rt");
+  public:
+    static std::string read_file(const char* filename)
+    {
+      unsigned long length;
+      char* data;
+      FILE* file = fopen(filename, "rt");
 
-    fseek(file, 0, SEEK_END);
-    length = ftell(file);
-    data = new char[length + 1];
-    memset(data, 0, length + 1);
-    fseek(file, 0, SEEK_SET);
-    fread(data, 1, length, file);
-    fclose(file);
+      fseek(file, 0, SEEK_END);
+      length = ftell(file);
+      data = new char[length + 1];
+      memset(data, 0, length + 1);
+      fseek(file, 0, SEEK_SET);
+      fread(data, 1, length, file);
+      fclose(file);
 
-    std::string result(data);
-    delete[] data;
+      std::string result(data);
+      delete[] data;
 
-    return result;
-  }
+      return result;
+    };
+  };
+
 } // ns yengine;
 
 #endif
